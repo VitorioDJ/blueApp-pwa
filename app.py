d02757bad5fab85cef7013e23b94be2f1f1a8c04
Vitorio De Janeiro <111264738+VitorioDJ@users.noreply.github.com>
2025-02-01 17:25:32 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__)
+
+@app.route('/sw.js')
+def serve_sw():
+    return send_file('sw.js', mimetype='application/javascript')
+
+@app.route('/manifest.json')
+def serve_manifest():
+    return send_file('manifest.json', mimetype='application/manifest+json')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run()
